a75da753c9ce5d953b1d70a4a783944860be8dc9
silverfox516 <silverfox516@hotmail.com>
2023-09-28 19:11:54 +0900

    1. don't check sourced already

--- envsetup.sh
+++ envsetup.sh
@@ -2,11 +2,6 @@
 
 BS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-if [[ $(declare -p BS_PRJ 2>&1) != *"not found"* ]]; then
-    echo "BS_PRJ=${BS_PRJ}, run in a new shell"
-    return
-fi
-
 export BS_PRJ=example
 BS_DIR_TARGETS=${BS_DIR}/${BS_PRJ}/targets
 BS_DIR_MODELS=${BS_DIR}/${BS_PRJ}/models
